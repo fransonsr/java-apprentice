dcf19fff58b5fdc3c01c4ae0e35b2000e96f5f83
fransonsr <fransonsr@familysearch.org>
2014-04-04 15:00:42 -0600

    Reworked the sorting example.

--- core-java/src/test/java/fransonsr/sorting/SortingTest.java
+++ core-java/src/test/java/fransonsr/sorting/SortingTest.java
@@ -1,11 +1,10 @@
 package fransonsr.sorting;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
@@ -15,77 +14,155 @@ import java.util.Random;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import fransonsr.utils.MD5Hash;
-
 public class SortingTest {
 
     @Ignore
     @Test
     public void generateRandomList() throws Exception {
+
+        List<String> list = new ArrayList<>();
+
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(getClass()
+            .getResourceAsStream("/wordsEn.txt")))) {
+            String line = null;
+
+            do {
+                line = in.readLine();
+                if (line != null) {
+                    list.add(line);
+                }
+            }
+            while (line != null);
+        }
+
         Random random = new Random();
 
-        for(int i = 0; i < 10000; i++) {
-            int nextInt = random.nextInt(100000);
+        for (int i = 0; i < 10000; i++) {
+            int nextInt = random.nextInt(list.size());
 
-            MD5Hash hash = new MD5Hash(Integer.toString(nextInt).toCharArray());
-
-            System.out.println(hash);
+            System.out.println(list.get(nextInt));
+            list.remove(nextInt);
         }
     }
 
     @Test
-    public void sortFile_asc() throws Exception {
-        sortFile(new Comparator<String>() {
+    public void sortFile_asc_Collections_Comparator() throws Exception {
+        final Comparator<String> ascComparator = new Comparator<String>() {
             public int compare(String string1, String string2) {
                 return string1.compareTo(string2);
+            }
+        };
+
+        System.out.println("sortFile_asc_Collections_Comparator:\n");
+
+        List<String> list = readListFromFile();
+
+        dumpList(list, new ListModifier() {
+            @Override
+            public void modify(List<String> list) {
+                Collections.sort(list, ascComparator);
             }
         });
     }
 
     @Test
-    public void sortFile_desc() throws Exception {
-        sortFile(new Comparator<String>() {
+    public void sortFile_desc_Collections_Comparator() throws Exception {
+        final Comparator<String> descComparator = new Comparator<String>() {
             public int compare(String string1, String string2) {
                 return string2.compareTo(string1);
+            }
+        };
+
+        System.out.println("sortFile_desc_Collections_Comparator:\n");
+
+        List<String> list = readListFromFile();
+
+        dumpList(list, new ListModifier() {
+            @Override
+            public void modify(List<String> list) {
+                Collections.sort(list, descComparator);
             }
         });
     }
 
-    private void sortFile(Comparator<String> comparator) throws IOException {
+    @Test
+    public void sortFile_asc_Collections_sort() throws Exception {
+
+        System.out.println("sortFile_asc_Collections_sort:\n");
+
+        List<String> list = readListFromFile();
+
+        dumpList(list, new ListModifier() {
+            @Override
+            public void modify(List<String> list) {
+                Collections.sort(list);
+            }
+        });
+    }
+
+    @Test
+    public void sortFile_asc_Collections_reverse() throws Exception {
+
+        System.out.println("sortFile_asc_Collections_reverse:\n");
+
+        List<String> list = readListFromFile();
+
+        dumpList(list, new ListModifier() {
+            @Override
+            public void modify(List<String> list) {
+                Collections.sort(list);
+                Collections.reverse(list);
+            }
+        });
+    }
+
+    private static interface ListModifier {
+        void modify(List<String> list);
+    }
+
+    private void dumpList(List<String> list, ListModifier listModifier) {
+        StringBuilder buff = new StringBuilder()
+            .append("Original (100 lines):\n")
+            .append(dumpList(list, 100));
+
+        System.out.println(buff.toString());
+
+        listModifier.modify(list);
+
+        buff = new StringBuilder()
+            .append("Modfied (100 lines):\n")
+            .append(dumpList(list, 100));
+
+        System.out.println(buff.toString());
+    }
+
+    private String dumpList(List<String> list, int size) {
+        list = list.subList(0, size);
+        StringBuilder buff = new StringBuilder();
+        for (String line : list) {
+            buff.append("\t").append(line).append("\n");
+        }
+
+        return buff.toString();
+    }
+
+    private List<String> readListFromFile() throws IOException {
         List<String> list = new LinkedList<String>();
 
         // read the list
         try (InputStream inputStream = getClass().getResourceAsStream("/unsortedList.txt");
              BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            
+
             String line;
             do {
                 line = reader.readLine();
-                if(line != null) {
+                if (line != null) {
                     list.add(line);
                 }
             }
-            while(line != null);
+            while (line != null);
         }
-
-        // sort the list
-        Collections.sort(list, comparator);
-
-        // save the file
-        File outputFile = File.createTempFile("sorted-asc", null);
-
-        System.out.println("Filename: " + outputFile.getCanonicalPath());
-
-        FileWriter writer = new FileWriter(outputFile);
-
-        try {
-            for(String line : list) {
-                writer.append(line).append("\n");
-            }
-        }
-        finally {
-            writer.close();
-        }
+        return list;
     }
 
 }
